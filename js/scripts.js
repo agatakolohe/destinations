8c7f0d6a733220284afb7d5edeeceec617b10955
agatakolohe <agatakolohe@gmail.com>
2020-11-02 21:46:14 -0800

    create funtions to display inputted trips and content, style in css

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,17 @@ TripLog.prototype.assignId = function () {
   return this.currentId;
 };
 
+TripLog.prototype.findTrip = function (id) {
+  for (let i = 0; i < this.destinations.length; i++) {
+    if (this.destinations[i]) {
+      if (this.destinations[i].id == id) {
+        return this.destinations[i];
+      }
+    }
+  }
+  return false;
+};
+
 // Business Logic for Destinations -----
 
 function Destination(city, food, landmark) {
@@ -36,14 +47,46 @@ Destination.prototype.displayDestination = function () {
 // User Interface Logic --------
 let tripLog = new TripLog();
 
+function displayTripDetails(tripLogToDisplay) {
+  let tripList = $("ul#destinations");
+  let htmlForTripInfo = "";
+  tripLogToDisplay.destinations.forEach(function (destination) {
+    htmlForTripInfo +=
+      "<li id=" + destination.id + ">" + destination.city + "</li>";
+  });
+  tripList.html(htmlForTripInfo);
+}
+function showTrip(tripId) {
+  const trip = tripLog.findTrip(tripId);
+  $("#show-destination").show();
+  $(".city-name").html(trip.city);
+  $(".famous-food").html(trip.food);
+  $(".famous-landmark").html(trip.landmark);
+  //let buttons = $("#buttons");
+  //buttons.empty();
+  //buttons.append(
+  //"<button class='deleteButton' id=" + +trip.id + ">Delete</button>" //need to create delete function for this to actually be of some use, or have the button have a different purpose
+  //);
+}
+function attachTripListeners() {
+  $("ul#destinations").on("click", "li", function () {
+    showTrip(this.id);
+  });
+}
+
 $(document).ready(function () {
+  attachTripListeners();
   $("form#new-trip").submit(function (event) {
     event.preventDefault();
     const inputtedCity = $("input#new-city").val();
     const inputtedFood = $("input#new-food").val();
     const inputtedLandmark = $("input#landmark").val();
+    $("input#new-city").val("");
+    $("input#new-food").val("");
+    $("input#landmark").val("");
+
     let newTrip = new Destination(inputtedCity, inputtedFood, inputtedLandmark);
     tripLog.addDestination(newTrip);
-    console.log(tripLog.destinations);
+    displayTripDetails(tripLog);
   });
 });
